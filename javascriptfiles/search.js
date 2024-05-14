5fe55707360b33d9f04ccbb933ff5a583b8b2002
afraahg <g.afraah@gmail.com>
2024-05-14 19:51:18 -0400

    warning, need to normalize line endings for unix/linux

--- javascriptfiles/search.js
+++ javascriptfiles/search.js
@@ -1,80 +1,36 @@
-let queriedWord = "";
-
 async function searchWord(){
-  //searchItem=document.searchForm.searchbar.value;
-  definitionPart = document.getElementById("definitions");
-  definitionPart.innerHTML = ""
-  let PoSarr = [];
-
-  getWord= await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/Hello`).then((res) =>res.json())
-    wordNeeded = getWord[0].word;
-    document.getElementById("wordTitle").innerHTML = wordNeeded;
-    for(let i = 0; i < getWord[0].meanings.length; i++){
-        PoSarr.push(getWord[0].meanings[i].partOfSpeech + "");
-        
-        const PoS = document.createElement("h3");
-        const PoSnode = document.createTextNode(getWord[0].meanings[i].partOfSpeech);
-        PoS.appendChild(PoSnode);
-
-        const def = document.createElement("p");
-        const defNode = document.createTextNode(getWord[0].meanings[i].definitions[0].definition);
-        def.appendChild(defNode);
-
-        definitionPart.appendChild(PoS);
-        definitionPart.appendChild(def);
-        
-        if((getWord[0].meanings[i].definitions[0].example + "") != "undefined"){
-            const sentenceUse = document.createElement("p");
-            const sentenceUseNode = document.createTextNode(getWord[0].meanings[i].definitions[0].example);
-            
-            sentenceUse.appendChild(sentenceUseNode);
-            definitionPart.appendChild(sentenceUse);
-        }
-        
-
-        
-        
-    }
-    document.getElementById("partsofspeech").innerHTML = "(" + PoSarr + ")";
-    
-
-      
-  //get values from API: returns array of dictionaries with 1 or more definitions
-
-  /* Keys from res array:
-      "word"
-      "meanings": [array of dictionaries]
-          contains>>>
-          "partsOfSpeech"
-          "definitions": [array of dictionaries]
-              contains>>>
-              "definition"
-              "example" (optional)
-          "synonyms":[]
-          "antonyms":[]
-          "sourceUrls":[url,url...]
-
-  */
-  //find the number of words (defined) and returned from the API
-  for (i=0; i<res.length; i++){
-      //create box on page for each word,
-
-          //then within each box sift through array of dictionarries corresponding to parts of speech/ multiple defintions
+  let getWord="";
+  const searchItem = document.searchForm.searchbar.value;
+  if (!searchItem){
+    console.log("no input");
+    return false;
   }
-  
-  /*console.log(res);
-  }).catch(error=>{
-      
-      console.log("did not get word");
-  });*/
+  try {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchItem}`);
+    getWord = await response.json();
+    
+    if (!getWord) {
+      console.log('word not found');
+      return;
+    }
+    
+    console.log('reached2');
+  } catch (error) {
+    console.log('error');
+    return;
+  }
+
+  // Save data to cache 
+  localStorage.setItem('wordData', JSON.stringify(getWord));
+  localStorage.setItem('searchItem', searchItem);
+
+
+  // Change location to browse
+  window.location.href = '/htmlfiles/browse.html';
 }
 
-// window.location.href='';
+document.searchForm.addEventListener('submit', (event) => {
 
-
-async function wordToLearn(){
-
-}
-
-searchWord();
-//window.onload=wordToLearn();
+  searchWord();
+  console.log('reached');
+});
